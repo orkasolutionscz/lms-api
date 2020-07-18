37fb2c99f867c46eae769da96ba5876abf4ea97a
jiri.fait <fait@orkasolutions.cz>
2020-07-18 14:17:14 +0200

    Uprava konfigu pro MySQL

---
+++ app/core/pagination.py
@@ -0,0 +1,18 @@
+from rest_framework import pagination
+from rest_framework.response import Response
+from collections import OrderedDict
+
+
+class CustomLimitPagination(pagination.PageNumberPagination):
+    page_size = 25
+    page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+             ('limit', self.page_size),
+             ('count', self.page.paginator.count),
+             ('page', self.page.number),
+             ('next', self.get_next_link()),
+             ('previous', self.get_previous_link()),
+             ('results', data)
+         ]))
