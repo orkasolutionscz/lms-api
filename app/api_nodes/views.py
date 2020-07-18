ad9902361b59db72570b97ad0ba8a767aa2c810d
jiri.fait <fait@orkasolutions.cz>
2020-07-18 18:10:32 +0200

    Added Netdevice and Models

--- app/api_nodes/views.py
+++ app/api_nodes/views.py
@@ -7,3 +7,15 @@ class NodeViewSet(BaseLmsApiAttrViewSet):
     """Manage tags in the database"""
     queryset = Nodes.objects.all()
     serializer_class = NodesSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Nodes.objects.all().order_by('id')
+        owner_id = self.request.query_params.get('ownerid', None)
+        if owner_id is not None:
+            queryset = queryset.filter(ownerid=owner_id)
+
+        return queryset
