1cf90c2f856fe830e38913c25a570702f339a109
jiri.fait <fait@orkasolutions.cz>
2020-08-01 23:41:55 +0200

    Pridani Ordering pro Cash

--- app/api_cash/views.py
+++ app/api_cash/views.py
@@ -21,9 +21,12 @@ class CashViewSetPages(BaseLmsApiAttrViewSet):
     - Vybirame jen typ 4 coz jsou platby z Banky
     - Setridime nenovejsim datem na zacatek
     - Strankovani
+    - Hledani customera: cash/pages/?customerid=1
+    - hledani cisla uctu: cash/pages/?comment__contains=1643321013/0800
     """
     queryset = Cash.objects.all().filter(type=4).order_by('-time')
     serializer_class = CashSerializer
     filter_backends = [DjangoFilterBackend]
     filter_fields = ['customerid', 'comment', 'value']
+    ordering_fields = ['customerid', 'value', 'platba_datum']
     pagination_class = CustomLimitPagination
