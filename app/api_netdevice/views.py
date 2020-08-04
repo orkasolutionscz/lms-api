5818d217615e10bfb997b794d4cfc866b70e571c
jiri.fait <fait@orkasolutions.cz>
2020-08-05 00:14:14 +0200

    Addd nms_group_filter

--- app/api_netdevice/views.py
+++ app/api_netdevice/views.py
@@ -7,3 +7,17 @@ class DevicesViewSet(BaseLmsApiAttrViewSet):
     """Manage tags in the database"""
     queryset = Netdevices.objects.all()
     serializer_class = NetdeviceSerializer
+
+    def get_queryset(self):
+        """
+        Zakladni filtr
+        /?nms_group=xyz
+        Vrati nam seznam netdevices pro nms_group xyz
+        """
+        queryset = Netdevices.objects.all().order_by('id')
+        nms_group_id = self.request.query_params.get('nms_group', None)
+        if nms_group_id is not None:
+            queryset = queryset.filter(nms_group=nms_group_id)
+
+        return queryset
+
--- app/api_nodes/views.py
+++ app/api_nodes/views.py
@@ -8,15 +8,3 @@ class NodeViewSet(BaseLmsApiAttrViewSet):
     queryset = Nodes.objects.all()
     serializer_class = NodesSerializer
 
-    def get_queryset(self):
-        """
-        Zakladni filtr
-        /Nodes?ownerid=xyz
-        Vrati nam seznam pocitacu pro usera s id xyz
-        """
-        queryset = Nodes.objects.all().order_by('id')
-        owner_id = self.request.query_params.get('ownerid', None)
-        if owner_id is not None:
-            queryset = queryset.filter(ownerid=owner_id)
-
-        return queryset
