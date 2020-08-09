2e695129b8b3e9fee9428442d2610f973fa80baa
jiri.fait <fait@orkasolutions.cz>
2020-08-09 20:45:55 +0200

    Verze ce nejede

--- app/app/settings.py
+++ app/app/settings.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "django.contrib.staticfiles",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
