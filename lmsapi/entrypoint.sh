fbe3cf16c06b0114b8f2a121ae1544187ffa41b1
fait@orkasolutions.cz <fait@orkasolutions.cz>
2020-08-14 08:13:11 +0200

    Zakomentovani flush....tohle zpusobovalo vzmzyani dat v tabulkach rizenych Django

--- lmsapi/entrypoint.sh
+++ lmsapi/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-python manage.py flush --no-input
+# Tohle na vzcisti tabulky VSECHNY ktere jsou management!!!!!
+#python manage.py flush --no-input
+
 python manage.py migrate
 
 exec "$@"
