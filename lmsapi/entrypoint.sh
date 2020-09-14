9086d3ebc3c46a0721571255446d67cc8e29fbde
fait@orkasolutions.cz <fait@orkasolutions.cz>
2020-09-14 02:32:01 +0200

    Novz nginx

--- lmsapi/entrypoint.sh
+++ lmsapi/entrypoint.sh
@@ -4,7 +4,7 @@
 #python manage.py flush --no-input
 
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
 gunicorn core.wsgi:application --bind 0.0.0.0:80
 
