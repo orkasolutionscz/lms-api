a7fcecec768e7c9e878823f2ed7c22ab14aa7c14
jiri.fait <fait@orkasolutions.cz>
2021-03-25 15:46:24 +0100

    Oprava Macs serialiyer

--- lmsapi/api_nodes/serializers.py
+++ lmsapi/api_nodes/serializers.py
@@ -2,19 +2,20 @@ from rest_framework import serializers
 from .models import Nodes, Macs, BtIphistory
 
 
-class NodesSerializer(serializers.ModelSerializer):
-    nodesmacs = serializers.StringRelatedField(many=True)
-
-    class Meta:
-        model = Nodes
-        fields = '__all__'
-
-
 class MacsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Macs
         fields = '__all__'
+
+
+class NodesSerializer(serializers.ModelSerializer):
+    nodesmacs = MacsSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Nodes
+        fields = '__all__'
+
 
 
 class IpHistorySerializer(serializers.ModelSerializer):
