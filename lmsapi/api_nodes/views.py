96ced05aefe8af9a6f43ff4dc6acc0d9f6b71e0b
jiri.fait <fait@orkasolutions.cz>
2021-10-16 16:10:15 +0200

    Uprava hledani dle IP

--- lmsapi/api_nodes/views.py
+++ lmsapi/api_nodes/views.py
@@ -4,6 +4,7 @@ from core.views import BaseViewSet
 import core.iptools as iptools
 import ipaddress
 
+
 class NodeViewSet(BaseViewSet):
     """Manage Nodes in the database"""
     queryset = Nodes.objects.all()
@@ -21,16 +22,17 @@ class IpHistoryViewSet(BaseViewSet):
     Parametry pro vybery:
     ip - format 192.168.1.0 nebo 123456789
     """
-    queryset = BtIphistory.objects.all()
+    queryset = BtIphistory.objects.all().order_by('-datum')
     serializer_class = IpHistorySerializer
     filter_fields = ['ip', 'cid', 'typ']
+    ordering = ['-datum']
 
     def get_queryset(self):
         """
         Optionally restricts the returned purchases to a given user,
         by filtering against a `username` query parameter in the URL.
         """
-        queryset = BtIphistory.objects.all()
+        queryset = BtIphistory.objects.all().order_by('-datum')
         p_ip = self.request.query_params.get('ip', None)
         p_iptext = self.request.query_params.get('iptext', None)
         p_cid = self.request.query_params.get('cid', None)
