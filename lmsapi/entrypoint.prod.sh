86c8b707f0f7f234a20ac3a76d0156aa362313eb
jiri.fait <fait@orkasolutions.cz>
2021-11-22 20:20:27 +0100

    Asi funkcni celery

--- lmsapi/entrypoint.prod.sh
+++ lmsapi/entrypoint.prod.sh
@@ -3,10 +3,10 @@
 # Tohle na vzcisti tabulky VSECHNY ktere jsou management!!!!!
 #python manage.py flush --no-input
 
-#python manage.py makemigrations
+python manage.py makemigrations
 python manage.py migrate
-#python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
-gunicorn core.wsgi:application --bind 0.0.0.0:8000
+#gunicorn core.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
