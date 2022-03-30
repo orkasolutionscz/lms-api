017ff5f560bc95af33608c8d9b5f0a428657c565
spravce <faitKUA>
2022-03-30 04:39:14 +0200

    Pridani Django-extensions

--- lmsapi/scripts/list_users.py
+++ lmsapi/scripts/list_users.py
@@ -2,6 +2,6 @@ from api_user.models import LmsUsers
 
 
 def run():
-    users = LmsUsers.objects.all()
+    users = LmsUsers.objects.filter(deleted=0)
     for user in users:
         print(user.name)
