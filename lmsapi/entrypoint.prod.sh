a4edb03df6592befb6036ec471e880cfa28ea02e
spravce <piatkovinatojedrina>
2022-05-11 11:01:52 +0200

    oprava spusteni apilms

--- lmsapi/entrypoint.prod.sh
+++ lmsapi/entrypoint.prod.sh
@@ -7,6 +7,6 @@
 python manage.py migrate
 #python manage.py collectstatic --noinput
 
-gunicorn core.wsgi:application --bind 0.0.0.0:8000
+#gunicorn core.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
