b124bdca4f9eb2723f29a8d115713933a16b9101
spravce <faitKUA>
2022-05-11 21:15:36 +0200

    funkcni develop verze

--- lmsapi/entrypoint.sh
+++ lmsapi/entrypoint.sh
@@ -6,8 +6,5 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic  --no-input --clear
-#python manage.py runserver 0.0.0.0:8000
-
-#gunicorn core.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
