c7ad1911ee85093129dfafb9bba131a1a3c5bffe
spravce <piatkovinatojedrina>
2022-05-11 12:26:25 +0200

    pred zmenou na nginx

--- lmsapi/entrypoint.prod.sh
+++ lmsapi/entrypoint.prod.sh
@@ -5,7 +5,7 @@
 
 #python manage.py makemigrations
 python manage.py migrate
-#python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
 #gunicorn core.wsgi:application --bind 0.0.0.0:8000
 
